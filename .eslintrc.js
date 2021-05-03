942806bf75cc9e71f4bde1852d22b73504e408ed
KiKiKi <chaika.design@gmail.com>
2021-05-03 20:55:31 +0900

    :sparkles: Format.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,44 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        'prettier',
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module",
-        "tsconfigRootDir": __dirname,
-        "project": ["./tsconfig.json"],
+    ecmaVersion: 12,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "settings": {
-      "react": {
-        "version": "detect"
-      }
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-      'newline-before-return': 'error',
-      'no-console': 'warn',
-      'no-var': 'error',
-      'no-dupe-class-members': 'error',
-      'eol-last': ['error', 'always'],
-      'object-shorthand': ['error', 'always'],
-      'prefer-arrow-callback': 'error',
-      'prefer-const': 'error',
-      'prefer-spread': 'error',
-      'require-yield': 'error',
-      // React roules
-      'react/prop-types': 'off',
-    }
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-var': 'error',
+    'no-dupe-class-members': 'error',
+    'eol-last': ['error', 'always'],
+    'object-shorthand': ['error', 'always'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-spread': 'error',
+    'require-yield': 'error',
+    // React roules
+    'react/prop-types': 'off',
+  },
 };
