e462904b7b9c7d3e81e9dd6601b899f63077b107
KiKiKi <chaika.design@gmail.com>
2021-05-03 20:53:07 +0900

    :sparkles: Prettier settings.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  semi: true,
+};
