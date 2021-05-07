451bd6014cd0721ddf58ea713b43cd55cdaffc2e
KiKiKi <chaika.design@gmail.com>
2021-05-07 19:27:22 +0900

    :wolf: Implement add new todo.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,14 @@
 import { VFC } from 'react';
 import { useRecoilValue } from 'recoil';
 import { todoListState } from '../recoil/atoms/todoListState';
+import { TodoForm } from './TodoForm';
 
 export const TodoList: VFC = () => {
   const todoList = useRecoilValue(todoListState);
 
   return (
     <div className="todoList">
+      <TodoForm />
       <ul>
         {todoList.map(([id, item]) => {
           return (
