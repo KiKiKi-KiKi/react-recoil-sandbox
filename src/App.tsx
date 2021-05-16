13bd84f0e03818b8e82c32d3db0f2574762a5301
KiKiKi <chaika.design@gmail.com>
2021-05-17 00:35:36 +0900

    :tractor: move files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { VFC } from 'react';
 import { RecoilRoot } from 'recoil';
-import { TodoList } from './components/TodoList';
+import { TodoList } from './TodoList/components/TodoList';
 import './App.css';
 
 export const App: VFC = () => {
