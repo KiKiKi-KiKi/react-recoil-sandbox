43d4135f80aa5b6bf7ffe396afad57113c2d27fc
KiKiKi <chaika.design@gmail.com>
2021-05-18 23:11:03 +0900

    :tractor: ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { VFC } from 'react';
 import { RecoilRoot } from 'recoil';
 //import { TodoList } from './TodoList/components/TodoList';
-import { CounterApp } from './AsyncCounter/components/CounterApp';
+// import { CounterApp } from './AsyncCounter/components/CounterApp';
 import './App.css';
 
 export const App: VFC = () => {
   return (
     <RecoilRoot>
-      <div className="App">
-        <CounterApp />
-      </div>
+      <div className="App">MY APP</div>
     </RecoilRoot>
   );
 };
