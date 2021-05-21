7d8e7629e075697af766d18cd13c5f23264751f1
KiKiKi <chaika.design@gmail.com>
2021-05-21 23:01:24 +0900

    :cyclone: Add return type.

--- src/WeatherApp/hooks/useWeather.ts
+++ src/WeatherApp/hooks/useWeather.ts
@@ -1,7 +1,12 @@
 import { useRecoilValue } from 'recoil';
 import { weatherState } from '../recoil/selector/weatherState';
+import { Openweathermap } from '../models/openweathermap';
 
-export const useWeather = () => {
+interface IuseWeather {
+  weather: Openweathermap | undefined;
+}
+
+export const useWeather = (): IuseWeather => {
   const weather = useRecoilValue(weatherState);
   const errorMessage = weather?.message;
   console.log({ weather });
